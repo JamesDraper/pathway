8ec109ae0fa691a81647b0ed697ecf7b6cf9427f
James Draper <>
2025-10-11 14:00:31 +0100

    Add tests to handler class to ensure that errors are thrown upwards.

--- tests/Unit/Internal/Reflection/HandlerClassTest.php
+++ tests/Unit/Internal/Reflection/HandlerClassTest.php
@@ -9,10 +9,12 @@ use Pathway\DispatcherInterface;
 
 use Tests\TestCase;
 
+use PHPUnit\Framework\Attributes\Depends;
 use PHPUnit\Framework\Attributes\Test;
 
 use Mockery;
 
+use LogicException;
 use stdClass;
 
 final class HandlerClassTest extends TestCase
@@ -49,5 +51,94 @@ final class HandlerClassTest extends TestCase
         $result = $handler->handle($message, $dispatcher);
 
         $this->assertSame(['i' => 'j', 'k' => 'l'], $result);
+    }
+
+    #[Test]
+    #[Depends('it_runs_prepare_process_and_finalize')]
+    public function exceptions_from_prepare_are_thrown_upwards(): void
+    {
+        $this->expectException(LogicException::class);
+
+        $message = new stdClass();
+
+        $dispatcher = Mockery::mock(DispatcherInterface::class);
+
+        $prepare = Mockery::mock(HandlerMethod::class);
+        $process = Mockery::mock(HandlerMethod::class);
+        $finalize = Mockery::mock(HandlerMethod::class);
+
+        $prepare
+            ->expects()
+            ->invoke([$message, $dispatcher])
+            ->andThrows(new LogicException);
+
+        $handler = new HandlerClass($prepare, $process, $finalize);
+
+        $handler->handle($message, $dispatcher);
+    }
+
+    #[Test]
+    #[Depends('it_runs_prepare_process_and_finalize')]
+    public function exceptions_from_process_are_thrown_upwards(): void
+    {
+        $this->expectException(LogicException::class);
+
+        $message = new stdClass();
+
+        $dispatcher = Mockery::mock(DispatcherInterface::class);
+
+        $prepare = Mockery::mock(HandlerMethod::class);
+        $process = Mockery::mock(HandlerMethod::class);
+        $finalize = Mockery::mock(HandlerMethod::class);
+
+        $prepare
+            ->expects()
+            ->invoke([$message, $dispatcher])
+            ->andReturn(['a' => 'b', 'c' => 'd']);
+
+        $process
+            ->expects()
+            ->invoke(['a' => 'b', 'c' => 'd'])
+            ->andThrows(new LogicException);
+
+
+        $handler = new HandlerClass($prepare, $process, $finalize);
+
+        $handler->handle($message, $dispatcher);
+    }
+
+    #[Test]
+    #[Depends('it_runs_prepare_process_and_finalize')]
+    public function exceptions_from_finalize_are_thrown_upwards(): void
+    {
+        $this->expectException(LogicException::class);
+
+        $message = new stdClass();
+
+        $dispatcher = Mockery::mock(DispatcherInterface::class);
+
+        $prepare = Mockery::mock(HandlerMethod::class);
+        $process = Mockery::mock(HandlerMethod::class);
+        $finalize = Mockery::mock(HandlerMethod::class);
+
+        $prepare
+            ->expects()
+            ->invoke([$message, $dispatcher])
+            ->andReturn(['a' => 'b', 'c' => 'd']);
+
+        $process
+            ->expects()
+            ->invoke(['a' => 'b', 'c' => 'd'])
+            ->andReturn(['e' => 'f', 'g' => 'h']);
+
+
+        $finalize
+            ->expects()
+            ->invoke(['e' => 'f', 'g' => 'h'])
+            ->andThrows(new LogicException);
+
+        $handler = new HandlerClass($prepare, $process, $finalize);
+
+        $handler->handle($message, $dispatcher);
     }
 }
