7c4a2d5b7c84c6a8247fc905f154ff3003f6a7fe
James Draper <>
2025-10-11 12:22:08 +0100

    Add outline of dispatcher class.

---
+++ src/Dispatcher.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Pathway;
+
+final class Dispatcher implements DispatcherInterface
+{
+    public function command(object $command): mixed
+    {
+        //
+    }
+
+    public function event(object $event): void
+    {
+        //
+    }
+}
