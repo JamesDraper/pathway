a7139146a2d421913c20f46ed8852d9515fbacfd
James Draper <>
2025-10-20 17:45:55 +0100

    corrected invalid namespace.

--- tests/HandlerRunner/HandlerRunnerTest.php
+++ tests/HandlerRunner/HandlerRunnerTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Tests\Unit\Internal\Handler;
+namespace Tests\HandlerRunner;
 
 use Pathway\HandlerRunner\HandlerRunner;
 use Pathway\HandlerRunner\Method;
