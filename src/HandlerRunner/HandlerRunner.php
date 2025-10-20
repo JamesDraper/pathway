290015486920aa4c702a9483950cf85d3bc713ec
James Draper <>
2025-10-20 17:27:23 +0100

    Added new handler runner class under handler runner namespace which is mostly Pathway\Internal\Handler\Handler copied.

---
+++ src/HandlerRunner/HandlerRunner.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+namespace Pathway\HandlerRunner;
+
+use Pathway\DispatcherInterface;
+
+/**
+ * @internal
+ * @template THandler of object
+ * @phpstan-type ArgumentList array<string, mixed>|list<mixed>
+ */
+class HandlerRunner
+{
+    /**
+     * @param Method<THandler> $prepare
+     * @param Method<THandler> $process
+     * @param Method<THandler> $finalize
+     */
+    public function __construct(
+        public readonly Method $prepare,
+        public readonly Method $process,
+        public readonly Method $finalize,
+    ) {
+    }
+
+    public function run(object $message, DispatcherInterface $dispatcher): mixed
+    {
+        /**
+         * @var ArgumentList $prepared
+         */
+        $prepared = $this->prepare->invoke([$message, $dispatcher]);
+
+        /**
+         * @var ArgumentList $processed
+         */
+        $processed = $this->process->invoke($prepared);
+
+        return $this->finalize->invoke($processed);
+    }
+}
---
+++ tests/HandlerRunner/HandlerRunnerTest.php
@@ -0,0 +1,145 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Unit\Internal\Handler;
+
+use Pathway\HandlerRunner\HandlerRunner;
+use Pathway\HandlerRunner\Method;
+use Pathway\DispatcherInterface;
+
+use Tests\TestCase;
+
+use PHPUnit\Framework\Attributes\Depends;
+use PHPUnit\Framework\Attributes\Test;
+
+use Mockery\MockInterface;
+use Mockery;
+
+use LogicException;
+use stdClass;
+
+final class HandlerRunnerTest extends TestCase
+{
+    private readonly HandlerRunner $handlerRunner;
+
+    private readonly DispatcherInterface&MockInterface $dispatcher;
+
+    private readonly Method&MockInterface $prepare;
+
+    private readonly Method&MockInterface $process;
+
+    private readonly Method&MockInterface $finalize;
+
+    #[Test]
+    public function it_runs_prepare_process_and_finalize(): void
+    {
+        $message = new stdClass();
+
+        $this
+            ->prepare
+            ->expects()
+            ->invoke([$message, $this->dispatcher])
+            ->andReturn(['a' => 'b', 'c' => 'd']);
+
+        $this
+            ->process
+            ->expects()
+            ->invoke(['a' => 'b', 'c' => 'd'])
+            ->andReturn(['e' => 'f', 'g' => 'h']);
+
+
+        $this
+            ->finalize
+            ->expects()
+            ->invoke(['e' => 'f', 'g' => 'h'])
+            ->andReturn(['i' => 'j', 'k' => 'l']);
+
+        $result = $this->handlerRunner->run($message, $this->dispatcher);
+
+        $this->assertSame(['i' => 'j', 'k' => 'l'], $result);
+    }
+
+    #[Test]
+    #[Depends('it_runs_prepare_process_and_finalize')]
+    public function exceptions_from_prepare_are_thrown_upwards(): void
+    {
+        $this->expectException(LogicException::class);
+
+        $message = new stdClass();
+
+        $this
+            ->prepare
+            ->expects()
+            ->invoke([$message, $this->dispatcher])
+            ->andThrows(new LogicException);
+
+        $this->handlerRunner->run($message, $this->dispatcher);
+    }
+
+    #[Test]
+    #[Depends('it_runs_prepare_process_and_finalize')]
+    public function exceptions_from_process_are_thrown_upwards(): void
+    {
+        $this->expectException(LogicException::class);
+
+        $message = new stdClass();
+
+        $this
+            ->prepare
+            ->expects()
+            ->invoke([$message, $this->dispatcher])
+            ->andReturn(['a' => 'b', 'c' => 'd']);
+
+        $this
+            ->process
+            ->expects()
+            ->invoke(['a' => 'b', 'c' => 'd'])
+            ->andThrows(new LogicException);
+
+
+        $this->handlerRunner->run($message, $this->dispatcher);
+    }
+
+    #[Test]
+    #[Depends('it_runs_prepare_process_and_finalize')]
+    public function exceptions_from_finalize_are_thrown_upwards(): void
+    {
+        $this->expectException(LogicException::class);
+
+        $message = new stdClass();
+
+        $this
+            ->prepare
+            ->expects()
+            ->invoke([$message, $this->dispatcher])
+            ->andReturn(['a' => 'b', 'c' => 'd']);
+
+        $this
+            ->process
+            ->expects()
+            ->invoke(['a' => 'b', 'c' => 'd'])
+            ->andReturn(['e' => 'f', 'g' => 'h']);
+
+
+        $this
+            ->finalize
+            ->expects()
+            ->invoke(['e' => 'f', 'g' => 'h'])
+            ->andThrows(new LogicException);
+
+        $this->handlerRunner->run($message, $this->dispatcher);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dispatcher = Mockery::mock(DispatcherInterface::class);
+
+        $this->prepare = Mockery::mock(Method::class);
+        $this->process = Mockery::mock(Method::class);
+        $this->finalize = Mockery::mock(Method::class);
+
+        $this->handlerRunner = new HandlerRunner($this->prepare, $this->process, $this->finalize);
+    }
+}
