19ae5469ca51b5ff2d4a0c98cdd4779fe9720282
James Draper <5493761+JamesDraper@users.noreply.github.com>
2026-06-01 22:59:45 +0100

    fix errors in class info factory and add in functionality to return null if class does not exist.

--- src/Internal/Info/Factory/ClassInfoFactory.php
+++ src/Internal/Info/Factory/ClassInfoFactory.php
@@ -6,14 +6,20 @@ namespace Pathway\Internal\Info\Factory;
 use Pathway\Internal\Info\Factory\MethodInfoFactory;
 use Pathway\Internal\Info\ClassInfo;
 
+use function class_exists;
+
 class ClassInfoFactory
 {
     public function __construct(private readonly MethodInfoFactory $methodInfoFactory)
     {
     }
 
-    public function make(string $class): MethodInfo
+    public function make(string $class): ?ClassInfo
     {
+        if (!class_exists($class)) {
+            return null;
+        }
+
         return new ClassInfo($this->methodInfoFactory, $class);
     }
 }
